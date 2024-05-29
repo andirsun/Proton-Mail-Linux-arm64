c5957efa4f9c96bde6a778e68adb4127cd8d18a3
Anderson Laverde <hola@andersonlaverde.com>
2024-05-29 09:16:25 +0100

    add released files

--- forge.config.ts
+++ forge.config.ts
@@ -1,5 +1,5 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-require("dotenv").config();
+require("dotenv").config({ path: ['.env', '.env.default'] });
 
 import { AutoUnpackNativesPlugin } from "@electron-forge/plugin-auto-unpack-natives";
 import { FusesPlugin } from "@electron-forge/plugin-fuses";
@@ -60,7 +60,7 @@ const config: ForgeConfig = {
                 iconUrl: `${__dirname}/assets/icons/${getIco()}`,
                 setupIcon: `${__dirname}/assets/icons/${getIco()}`,
                 loadingGif: `${__dirname}/assets/windows/install-spinner.gif`,
-                signWithParams: `/a /d "Proton Mail Desktop" /t "http://timestamp.sectigo.com" /fd SHA256`,
+                signWithParams: process.env.WINDOWS_PACKAGE_SIGNING === 'yes' ? `/a /d "Proton Mail Desktop" /t "http://timestamp.sectigo.com" /fd SHA256` : undefined,
             },
         },
         {
